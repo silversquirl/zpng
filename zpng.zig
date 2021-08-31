792cc24732b0f463abbdf51b93d3b0c17013daa3
Samadi van Koten <samadi@vktec.org.uk>
2021-08-31 22:47:55 +0100

    Add extra test images

--- zpng.zig
+++ zpng.zig
@@ -364,15 +364,20 @@ fn paeth(a: u8, b: u8, c: u8) u8 {
 }
 
 test "read png" {
-    const f = try std.fs.cwd().openFile("test.png", .{});
-    defer f.close();
-    var buf = std.io.bufferedReader(f.reader());
-    const img = try Image.read(std.testing.allocator, buf.reader());
-    defer img.deinit(std.testing.allocator);
+    var dir = try std.fs.cwd().openDir("test", .{ .iterate = true });
+    defer dir.close();
+    var it = dir.iterate();
+    while (try it.next()) |entry| {
+        const f = try dir.openFile(entry.name, .{});
+        defer f.close();
+        var buf = std.io.bufferedReader(f.reader());
+        const img = try Image.read(std.testing.allocator, buf.reader());
+        defer img.deinit(std.testing.allocator);
 
-    for (img.pixels) |pix, i| {
-        const r: u16 = if (img.x(i) < 32) 0 else 65535;
-        const b: u16 = if (img.y(i) < 32) 0 else 65535;
-        try std.testing.expectEqual([4]u16{ r, 0, b, 65535 }, pix);
+        for (img.pixels) |pix, i| {
+            const r: u16 = if (img.x(i) < 32) 0 else 65535;
+            const b: u16 = if (img.y(i) < 32) 0 else 65535;
+            try std.testing.expectEqual([4]u16{ r, 0, b, 65535 }, pix);
+        }
     }
 }
